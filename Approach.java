79c82bd4861b17779660c3185b0d8395dd2358d4
Bhavy patel <88310262+Bhavy1800@users.noreply.github.com>
2024-04-21 22:15:06 +0530

    Add files via upload

---
+++ Approach.java
@@ -0,0 +1,1 @@
+public int Approach(int[] prices) {int buy = prices[0]; int profit = 0; for (int i=1; i<prices.length; i++) { if (buy < prices[i]) { profit = Math.max(prices[i] - buy, profit); } else { buy = prices[i]; } } return profit;
